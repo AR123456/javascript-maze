f4336bb2f238184a09210a9366efc37237cf9681
AR123456 <coloradoz99@gmail.com>
2020-01-09 04:33:50 -0700

    getting content to appear

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi there!');
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+// boilerplate code related to Matter JS
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body, // addatvie doesnt destroy
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600
+  }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+///////////////////// end of boilerplate
+
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+  isStatic: true
+});
+World.add(world, shape);
