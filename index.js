06284c83f67868f2ceb8120ddb8d04bec7d3a5a2
AR123456 <coloradoz99@gmail.com>
2020-01-14 06:27:49 -0700

    fun demo for Nora

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const {
   Mouse
 } = Matter;
 // boilerplate code related to Matter JS
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    // wireframes: false, // if false shapes will be solid colors
+    width,
+    height
   }
 });
 Render.run(render);
@@ -35,5 +39,26 @@ const walls = [
   Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
 ];
 World.add(world, walls);
-
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random Shape generator
+for (let i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(
+        Math.random() * width,
+        Math.random() * height,
+        35
+        // to add a diffrent color use render
+        // Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+        //   render: {
+        //     fillStyle: "green"
+        //   }
+      )
+    );
+  }
+}
