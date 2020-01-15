14afb7d2515581f6102be0bd9dac782b7613c649
AR123456 <coloradoz99@gmail.com>
2020-01-15 07:12:16 -0700

    configure variables

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies,
-  MouseConstraint,
-  Mouse
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 // boilerplate code related to Matter JS
-const width = 800;
+const width = 600;
 const height = 600;
 const engine = Engine.create();
 const { world } = engine;
@@ -16,20 +8,18 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
+    wierframes: true,
     width,
     height
   }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
-//////////////Maze algorithm
-// create a grid of cells - 2 dimentional array
-// pick a random starting cell
-// for that cell build a randomly ordered list of neighbors
-// if a neighbor has been visited before, reomove it from the list
-// for each remaiing neighbor, "move " to it and remvoe the wall between those two cells
-// repeat this for new neighbor
-// to track where there are walls or not
-/// use vertical wall and horizontal wall types   - 2 dimentional arrys for vertical and horizontal
-// boolean true no wall ,  false there is a wall
- 
+//walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+];
+World.add(world, walls);
