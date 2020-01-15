7a3c0bdf2723e1fa35df4ec66aec45005d228902
AR123456 <coloradoz99@gmail.com>
2020-01-15 07:13:03 -0700

    grid generation map vs for

--- index.js
+++ index.js
@@ -23,3 +23,21 @@ const walls = [
   Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
+
+//Maze generation - using for loops
+// const grid = [];
+// for (let i = 0; i < 3; i++) {
+//   grid.push([]);
+//   for (let j = 0; j < 3; j++) {
+//     grid[i].push(false);
+//   }
+// }
+// console.log(grid);
+
+// Maze generation - better way using map
+// this is more scalable if later we wanted more rows and or columns can just increase the
+const grid = Array(3) //rows
+  .fill(null)
+  .map(() => Array(3).fill(false));//columns 
+
+console.log(grid);
